a58b74f2e2037feedebdade59da6dd944b5ae1c9
Nesar Ahmed Naeem <hello@nesaran.com>
2023-08-12 18:13:50 +0600

    progress bar removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,9 @@
-"use client";
 import Footer from "./components/footer/Footer";
 import NavBar from "./components/menu/header/NavBar";
 import SearchMenu from "./components/menu/searchMenu/SearchMenu";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import NextNProgress from "nextjs-progressbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -21,13 +20,7 @@ export default function RootLayout({ children }) {
         </div>
         <SearchMenu />
         <main className="bg-gray-700 py-5">{children}</main>
-        <NextNProgress
-          color="#29D"
-          startPosition={0.3}
-          stopDelayMs={200}
-          height={3}
-          showOnShallow={true}
-        />
+
         <Footer />
       </body>
     </html>
