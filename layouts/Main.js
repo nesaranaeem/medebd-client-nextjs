836150de87d6c7066f66ac64efe98c4f9442b28f
Nesar Ahmed Naeem <hello@nesaran.com>
2023-08-20 02:53:19 +0600

    footer updated

--- layouts/Main.js
+++ layouts/Main.js
@@ -5,7 +5,7 @@ import SearchMenu from "@/components/menu/searchMenu/SearchMenu";
 export default function MainLayout({ children }) {
   return (
     <>
-      <div className="sticky top-0 z-50">
+      <div className="sticky top-0 z-50" id="top">
         <NavBar />
       </div>
       <SearchMenu />
