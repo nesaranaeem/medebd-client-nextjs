fc2f98fd5ae83b55d3e1f9bfd1520c8d2399395d
Nesar Ahmed Naeem <hello@nesaran.com>
2023-09-20 04:21:09 +0600

    oprn graph updated

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,19 +3,19 @@ const NextSeo = {
   titleTemplate: "%s | Medicine Details",
   description: "Largest Online Medicine Index",
 
-  openGraph: {
-    type: "website",
-    url: "https://medebd.com",
-    site_name: "MedeBD",
-    images: [
-      {
-        url: "https://medebd.com/images/og-image.png",
-        width: 300,
-        height: 200,
-        alt: "MedeBD - Online Medicine Index",
-      },
-    ],
-  },
+  // openGraph: {
+  //   type: "website",
+  //   url: "https://medebd.com",
+  //   site_name: "MedeBD",
+  //   images: [
+  //     {
+  //       url: "https://medebd.com/images/og-image.png",
+  //       width: 300,
+  //       height: 200,
+  //       alt: "MedeBD - Online Medicine Index",
+  //     },
+  //   ],
+  // },
 };
 
 export default NextSeo;
